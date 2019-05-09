6bf10b7df28bf7c495d4b7b0a990a93927a30690
Aguniec <aguniecc@gmail.com>
2019-05-10 01:32:44 +0200

    df

--- Numerical integration.py
+++ Numerical integration.py
@@ -2,65 +2,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import integrate
 
-#metoda prostokątów
 
 def f(x):
-    return np.cos(x);
+    return np.cos(x)
 
-n=10#liczba punktów podziału
+
+def squares_method(n, a):
+    b = np.pi/2
+    dx = (b-a)/(n)
+    result = 0
+    for i in range(1, n-1):
+        result += f(a+i*dx) * dx
+    print("result for squares method : ", result, np.sin(b)-np.sin(a))
+
+
+def trapezoidal_rule(n, a):
+    b = np.pi/2
+    dx = (b-a)/(n)
+    result = 0
+    sum = 0
+    for i in range(1, n-1):
+        sum += f(a+i*dx)
+    result = f(a)+(2*sum)+f(b)
+    result = result*(b-a)/(2*n)
+    print("result for trapezoidal rule:  ", result, np.sin(b)-np.sin(a))
+
+
+def simpons_method(n, a, b):
+      dx = (b-a)/(n)
+      result, sum1, sum = 0, 0, 0
+      for i in range (1, n):
+            x = a+ i*dx
+            sum1 = sum1+ f(x-(dx/2))
+            if i < n:
+                  result = result + f(x)
+      result = f(a)+f(b)+2*result+4*sum1
+      result = result*(dx/6)
+      print("wynik met. simps. wynosi : ", result) 
+      
+squares_method(10, 0)
+trapezoidal_rule(10000, 0)
+simpons_method(10,0,1)
+
+
+
+# metoda Simpsona
+
+
+n = 10
 a = 0
-b = np.pi/2
-dx=(b-a)/(n)
-wynik=0
-for i in range (1,n-1):
-    wynik+=f(a+i*dx)*dx
+b = 1
 
-print("wynik met. prost. wynosi : ",wynik,np.sin(b)-np.sin(a))
-
-
-#metoda trapezów
-
-def f(x):
-    return np.cos(x);
-n=10000
-a=0
-b=np.pi/2
-dx=(b-a)/(n)
-wynik=0
-suma=0
-for i in range (1,n-1):
-    suma+=f(a+i*dx)
-wynik=f(a)+(2*suma)+f(b)
-wynik=wynik*(b-a)/(2*n)
-print("wynik met. trap. wynosi : ",wynik,np.sin(b)-np.sin(a) )
+wynik = 0
+suma1 = 0
+suma = 0
+wartosc = 0
 
 
-
-
-
-
-
-
-#metoda Simpsona
-     
-def f(x):
-  return np.cos(x);
-n=10
-a=0
-b=1
-dx=(b-a)/(n)
-wynik=0
-suma1=0
-suma=0
-wartosc=0
-
-for i in range (1,n):
-    x=a+i*dx
-    suma1=suma1+f(x-(dx/2))
-    if i<n:
-        wartosc=wartosc+f(x)
-wartosc=f(a)+f(b)+2*wartosc+4*suma1
-wartosc=wartosc*(dx/6)
-
-print ("wynik met. simps. wynosi : ",wartosc) 
 
